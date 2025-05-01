f1664a8a630119458fecee48af9487f166bd4e11
r_o_k_u <corneliusokaya@gmail.com>
2025-05-01 14:17:21 +0300

    cleanup

--- server_old/app/motor_control.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-
-@app.route('/motor_control')
-def motor_control():
-    # Simulate motor control data
-    motor_commands = ['forward', 'backward', 'left', 'right']
-    return render_template('motor_control.html', motor_commands=motor_commands)
--- server_old/app/video_feed.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-
-@app.route('/video_feed')
-def video_feed():
-    # Simulate video feed data
-    frame = '<img src="https://picsum.photos/200/300">'
-    return render_template('video_feed.html', frame=frame)
